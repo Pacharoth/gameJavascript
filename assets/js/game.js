40b6355a1ff5827d6ccf1827193b2e95082d0da9
roeun <pacharothroeun01@gmail.com>
2020-12-05 22:45:17 +0700

    [TASK] create layout and component
    [x]clear function
    [x]make square
    [x]area startgame
    [x]onload listen
    []update game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,42 @@
-var canvas = document.getElementById("snakeGame")
+//Game start
+function startGame(){
+    myGameArea.start(); 
+    let snake = new makeSquare(30,30,10,120,"#ff7a3c").update();  
+}
+
+//create the area of the game
+// we dont use css because it will damage the picture
+let myGameArea = {
+    canvas: document.createElement("canvas"),
+    start:function(){
+        //the area of game play
+        this.canvas.width = 900;
+        this.canvas.height= 600;
+        this.canvas.style.border="1px solid black";
+        this.canvas.style.margin = "auto";
+        this.canvas.style.display = "block";
+        this.context = this.canvas.getContext('2d');
+        
+        //insert to body 
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    },
+    //erase function
+    clear:function(){
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+       
+    }
+}
+
+//create square
+function makeSquare(width,height,x,y,color){
+    let context = myGameArea.context;
+    // create function update
+    this.update= function(){
+        context.fillStyle=color;
+        context.fillRect(x,y,width,height);
+    }
+}
+window.addEventListener("load",
+    startGame
+);
+
