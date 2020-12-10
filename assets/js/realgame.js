1dda2f54d9855e8769952a92c32d3d03b6dc4026
roeun <pacharothroeun01@gmail.com>
2020-12-10 12:23:35 +0700

    [realGame]

--- assets/js/realgame.js
+++ assets/js/realgame.js
@@ -4,8 +4,9 @@ canvas.width=500;
 canvas.height=400;
 canvas.style.border="1px solid black";
 document.body.append(canvas);
+canvas.tabIndex=1;
+canvas.id="hey"
 let context = canvas.getContext("2d")
-
 //control 
 let up=false,down=false,left=false,right=false
 
@@ -14,51 +15,68 @@ function earseRectangle(){
     context.fillRect(0,0,500,400);
 }
 
-
-function drawBall(x,y){
-    this.x = x;
-    this.y = y;
-    context.beginPath();
-    context.arc(x,y,10,0,Math.PI*2);
-    context.fillStyle="green";
-    this.update=()=>{
-        context.fill();
+//global viriable
+function drawBall(x,y,speed){
+    return{
+        x:x,
+        y:y,
+        s:speed,
+        update:function(){
+            context.beginPath(),
+            context.arc(this.x,this.y,10,0,Math.PI*2),
+            context.fillStyle="green",
+            context.fill();
+        }
     }
 }
-
-function drawRectangle(x,y,width,height){
-    this.x=x;
-    this.y=y;
-    this.width=width;
-    this.height=height;
-    this.update=()=>{
-        context.fillStyle="black";
-        context.fillRect(this.x,this.y,this.width,this.height);
+function drawRectangle(x,y,width,height,speed){
+    return{
+        x:x,
+        y:y,
+        s:speed,
+        width:width,
+        height:height,
+        update:function(){
+            context.fillStyle="black";
+            context.fillRect(this.x,this.y,this.width,this.height);
+        }
     }
 }
-function moveItem(item){
-    if(up)         item.y -= 1;
-    else if(down)  item.y += 1;
-    else if(left)  item.x -= 1;
+let ball = drawBall(20,30);
+let rectangle =drawRectangle(20,40,30,10,10)
+//between
+function isWithin(a,b,c){
+    return(b>a && b<c)
+}
+
+function isColliding(a,b,c){
+    let result = false;
+    // if(isWithin())
+}
+
+function moveBall(item){
+    
+    if(left)  item.x -= 1;
     else if(right) item.x += 1;
 }
+
 //start game
 function startGame(){
-    // earseRectangle();
-    context.clearRect(0,0,canvas.width,canvas.width)
-    ball = new drawBall(20,30);
-    ball.x+=1;
-    ball.y-=2;
-    ball.update();
-    rect= new drawRectangle(50,50,200,10);
-    rect.update();
+    // eraseRect();
+    context.clearRect(0,0,canvas.width,canvas.height);
+    rectangle.update()
+    moveBall(ball)
+    ball.update()
 }
-window.addEventListener("keyup",function(event){
+
+document.getElementById("hey").addEventListener("keydown",function(event){
     up=false,left=false,right=false,left=false
     if(event.key=="ArrowUp")           up  = true;
     else if(event.key=="ArrowDown")  down  = true;
     else if(event.key=="ArrowLeft")  left  = true;
     else if(event.key=="ArrowRight") right = true;
-  
+    
+    startGame();
 })
 startGame();
+setInterval()
